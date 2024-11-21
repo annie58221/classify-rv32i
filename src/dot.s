fc48f4798862d816bdebf1973eaf2c73ce290e18
annie58221 <hehe19950955@gmail.com>
2024-11-21 16:07:44 +0800

    commit

--- src/dot.s
+++ src/dot.s
@@ -30,86 +30,39 @@ dot:
     blt a2, t0, error_terminate  
     blt a3, t0, error_terminate   
     blt a4, t0, error_terminate  
+    
+    li t0, 0           
+    li t1, 0         
 
-    li t0, 0            
-    li t1, 0               # counter
-    addi t2, x0, 2         # add
-    sll a3, a3, t2         # add
-    sll a4, a4, t2         # add
-
+    slli t4, a3, 2
+    slli t5, a4, 2
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
+    lw t2, 0(a0)
+    lw t3, 0(a1)
     
-    mv t3, t1                # mul t2, t1, a3
-    mv t6, a3  
-    li t4, 0
-    li t5, 32
-    li t2, 0  
-loop:
-    bge t4, t5, end_loop
-    addi t4, t4, 1
-    andi a7, t6, 1
-    srli t6, t6, 1
-    bnez a7, add_to_result
-    slli t3, t3, 1
-    j loop
-add_to_result:
-    add t2, t2, t3
-    slli t3, t3, 1
-    j loop
-end_loop:
+    beqz t2, zero
+    beqz t3, zero
 
-    mv a6, t1             # mul t3, t1, a4
-    mv t6, a4   
-    li t4, 0
-    li t5, 32
-    li t3, 0   
-loop_2:
-    bge t4, t5, end_loop_2
-    addi t4, t4, 1
-    andi a7, t6, 1
-    srli t6, t6, 1
-    bnez a7, add_to_result_2
-    slli a6, a6, 1
-    j loop_2
-add_to_result_2:
-    add t3, t3, a6
-    slli a6, a6, 1
-    j loop_2
-end_loop_2:    
+    bge  t3, x0, my_mul
+    xori t3, t3, -1
+    sub  t2, x0, t2
+    addi t3, t3, 1
+my_mul:
+    andi t6, t3, 1
+    beq  t6, x0, continue
+    add  t0, t0, t2
+continue:
+    slli t2, t2, 1
+    srli t3, t3, 1
+    bnez t3, my_mul
+zero:    
+    addi t1, t1, 1
+    add  a0, a0, t4
+    add  a1, a1, t5
+    j    loop_start
 
-    
-    add t2, a0, t2           # t4 = array0 + counter * stride0 * sizeof(int)
-    add t3, a1, t3           # t5 = array1 + counter * stride1 * sizeof(int)
-    lw t2, 0(t2)             # t4 = array0[counter]
-    lw t3, 0(t3)             # t5 = array1[counter]
-        
-    mv a6, t2                # mul t4, t2, t3
-    mv t6, t3   
-    li t2, 0
-    li t5, 32
-    li t4, 0   
-loop_3:
-    bge t2, t5, end_loop_3
-    addi t2, t2, 1
-    andi a7, t6, 1
-    srli t6, t6, 1
-    bnez a7, add_to_result_3
-    slli a6, a6, 1
-    j loop_3
-
-add_to_result_3:
-    add t4, t4, a6
-    slli a6, a6, 1
-    j loop_3
-end_loop_3:     
-    add t0, t0, t4           # product = product + array0[counter] * array[counter]
-    addi t1, t1, 1           # counter = counter + 1
-    j loop_start
-    
-    
-    
 loop_end:
     mv a0, t0
     jr ra
